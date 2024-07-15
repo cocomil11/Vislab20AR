a3aace8717d0c5f42beac05f11bb8fb6581bab4f
CeraChen <1059315177@qq.com>
2024-07-15 15:10:00 +0800

    initialize ui for photos and line chart

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,59 @@ import React, { useState } from "react";
 import "./assets/css/App.css"; // import MindARViewer from "./mindar-viewer";
 import ArComponent from "./ArComponent";
 
+import logo from './vislab_logo.png';
+import photo2023 from './photos/VISLab2023.png';
+import LineChart from './LineChart.js';
+import "./assets/css/ui.css"
+
+
+var mSVG = <svg className="icon" viewBox="0 0 1194 1024"><path d="M481.457 880.601l-89.86-81.301 263.168-290.983-263.168-290.983 89.86-81.301 335.913 372.283z" p-id="5100"></path></svg>
+
+const Slider: React.FC<{ currentYear: number }> = ({ currentYear }) => {
+  var progress = (currentYear - 2004)/20 * 100;
+
+  return (
+    <div className="slider">
+      <div className="slider-thumb" style={{ left: `${progress}%`}}></div>
+      <div className="slider-annotation" style={{ left: `${progress}%`}}>{currentYear.toString()}</div>
+    </div>
+  );
+}
+
+
+
+
+
 const App: React.FC = () => {
   const [started, setStarted] = useState<string | null>(null);
+  var year = 2004;
 
   return (
     <div className="App">
-      <h1>PEP : READY, GET SET, GO!</h1>
+      <div className="heading">
+              <img src={logo} className="heading-logo" alt="logo" />
+              <div className="heading-title">VisLab 20th Anniversary</div>
+          </div>
+          <div className="canvas">
+              <div className="photo-container">
+                  <span id="left-arrow">{mSVG}</span>
+                  <div className="photo-canvas">
+                      <img src={photo2023} className="photo" alt="photo" />
+                      <div className="photo-caption">VisLab {year.toString()}</div>
+                  </div>
+                  <span id="right-arrow">{mSVG}</span>
+              </div>
+
+              <div className="chart-container">
+                  <div className="chart">
+                      <LineChart currentYear={year}/>
+                  </div>
+                  <div className="slider-container">
+                      <Slider currentYear={year}></Slider>
+                  </div>
+              </div>
+          </div>
+      {/* <h1>PEP : READY, GET SET, GO!</h1>
 
       <div className="control-buttons">
         {started === null && (
@@ -35,7 +82,7 @@ const App: React.FC = () => {
           <ArComponent />
           <video></video>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
---
+++ src/LineChart.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import ReactEcharts from 'echarts-for-react';
+
+export default function LineChart(props) {
+    const currentYear = props.currentYear;
+    let data = {
+        categories: [
+            2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016,
+            2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+        ],
+        series: [
+            {
+                name: 'Total Pub',
+                type: 'line',
+                data: [0, 0, 0, 2, 4, 10, 12, 15, 17, 18, 21, 26, 35, 39, 42, 54, 58, 74, 87, 95, 110]
+            },
+            {
+                name: 'Total Ph.D.',
+                type: 'line',
+                data: [0, 0, 0, 0, 0, 3, 3, 4, 5, 6, 8, 8, 10, 11, 18, 26, 28, 30, 36, 41, 42]
+            },
+            {
+                name: 'Total MPhil',
+                type: 'line',
+                data: [0, 0, 1, 1, 2, 6, 6, 9, 10, 10, 11, 11, 14, 14, 17, 19, 22, 24, 28, 30, 30]
+            }
+        ]
+    };
+      
+    let option = {
+        xAxis: {
+            type: 'category',
+            data: data.categories,
+            axisLabel: {
+                interval: 4,
+                fontSize: 13, 
+                color: '#333'
+            },
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            offset: 10,
+        },
+        yAxis: {
+            type: 'value',
+            axisLabel: {
+                fontSize: 13, 
+                color: '#333'
+            },
+            axisLine: {
+                show: false
+            },
+            axisTick: {
+                show: false
+            },
+            splitLine: {
+                show: false
+            },
+            splitArea: {
+                show: false
+            }
+        },
+        series: [
+            {
+                name: data.series[0].name,
+                type: 'line',
+                data: data.series[0].data,
+                lineStyle: {
+                    color: '#5EA8D2',
+                    width: 3
+                },
+                itemStyle: {
+                    opacity: 0
+                },
+                smooth: true,
+                z: 10
+            },
+            {
+                name: data.series[1].name,
+                type: 'line',
+                data: data.series[1].data,
+                lineStyle: {
+                    color: '#38C4A2',
+                    width: 3
+                },
+                itemStyle: {
+                    opacity: 0
+                },
+                smooth: true,
+                z: 8
+            },
+            {
+                name: data.series[2].name,
+                type: 'line',
+                data: data.series[2].data,
+                lineStyle: {
+                    color: '#346A64',
+                    width: 3
+                },
+                itemStyle: {
+                    opacity: 0
+                },
+                smooth: true,
+                z: 6
+            }, 
+            {
+                name: "",
+                type: 'scatter',
+                data: [[currentYear.toString(), data.series[0].data[currentYear-2004]]],
+                symbol: 'emptyCircle',
+                symbolSize: 12,
+                itemStyle: {
+                    color: '#5EA8D2',
+                    borderWidth: 6,
+                    opacity: 1 
+                },
+                z: 12
+            }
+        ],
+        legend: {
+            orient: 'vertical',
+            left: 98,
+            top: 50,
+            color: "#333"
+        }
+    };
+
+    return (
+        <ReactEcharts
+            option={option}
+            // notMerge
+            // lazyUpdate
+            style={{ height: '100%', width: "100%", marginLeft: '3px'}} //, marginLeft: '-5px', paddingTop: '15px'}}
+        />
+    );
+}
