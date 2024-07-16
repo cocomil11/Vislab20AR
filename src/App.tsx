215405c5a2d8992d92addbd7c545b6a460dc240a
kentoglwo@gmail.com <kentoglwo@gmail.com>
2024-07-16 10:34:24 +0800

    :sparkles: show App via router

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./assets/css/App.css"; // import MindARViewer from "./mindar-viewer";
 import logo from './vislab_logo.png';
 import "./assets/css/ui.css"
-import ScanResult from "./ScanResult";
-
+import photo2023 from './photos/VISLab2023.png';
+import LineChart from './LineChart.js';
 
 var mSVG = <svg className="icon" viewBox="0 0 1194 1024"><path d="M481.457 880.601l-89.86-81.301 263.168-290.983-263.168-290.983 89.86-81.301 335.913 372.283z" p-id="5100"></path></svg>
 
@@ -18,50 +18,34 @@ const Slider: React.FC<{ currentYear: number }> = ({ currentYear }) => {
   );
 }
 
-
-
-
-
 const App: React.FC = () => {
   const [started, setStarted] = useState<string | null>(null);
   var year = 2023;
-
   return (
     <div className="App">
       <div className="heading">
           <img src={logo} className="heading-logo" alt="logo" />
           <div className="heading-title">VisLab 20th Anniversary</div>
       </div>
-      <ScanResult />
-      {/* <h1>PEP : READY, GET SET, GO!</h1>
+      <div className="canvas">
+          <div className="photo-container">
+              <span id="left-arrow">{mSVG}</span>
+              <div className="photo-canvas">
+                  <img src={photo2023} className="photo" alt="photo" />
+                  <div className="photo-caption">VisLab {year.toString()}</div>
+              </div>
+              <span id="right-arrow">{mSVG}</span>
+          </div>
 
-      <div className="control-buttons">
-        {started === null && (
-          <button
-            onClick={() => {
-              setStarted("aframe");
-            }}
-          >
-            Start Tracking
-          </button>
-        )}
-        {started !== null && (
-          <button
-            onClick={() => {
-              setStarted(null);
-            }}
-          >
-            Stop
-          </button>
-        )}
+          <div className="chart-container">
+              <div className="chart">
+                  <LineChart currentYear={year}/>
+              </div>
+              <div className="slider-container">
+                  <Slider currentYear={year}></Slider>
+              </div>
+          </div>
       </div>
-
-      {started === "aframe" && (
-        <div className="container">
-          <ArComponent />
-          <video></video>
-        </div>
-      )} */}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import "./assets/css/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
