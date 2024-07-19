341463bf8c10086897cbb248c35b6f587dbe1201
Kento <kentoglwo@gmail.com>
2024-07-19 20:36:17 +0800

    :sparkles: add pagenation for switching images in a year

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 import "./assets/css/App.css"; // import MindARViewer from "./mindar-viewer";
-import ArComponent from "./ArComponent";
-
 import logo from './vislab_logo.png';
+import "./assets/css/ui.css"
 import photo2023 from './photos/VISLab2023.png';
 import LineChart from './LineChart.js';
-import "./assets/css/ui.css"
-
+import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 var mSVG = <svg className="icon" viewBox="0 0 1194 1024"><path d="M481.457 880.601l-89.86-81.301 263.168-290.983-263.168-290.983 89.86-81.301 335.913 372.283z" p-id="5100"></path></svg>
 
@@ -21,68 +21,44 @@ const Slider: React.FC<{ currentYear: number }> = ({ currentYear }) => {
   );
 }
 
-
-
-
-
 const App: React.FC = () => {
   const [started, setStarted] = useState<string | null>(null);
-  var year = 2023;
+  const [year, setYear] = useState<number>(2014)
+  const [page, setPage] = useState<number>(1);
+
+  const handleChange = (event: React.ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   return (
     <div className="App">
       <div className="heading">
-              <img src={logo} className="heading-logo" alt="logo" />
-              <div className="heading-title">VisLab 20th Anniversary</div>
-          </div>
-          <div className="canvas">
-              <div className="photo-container">
-                  <span id="left-arrow">{mSVG}</span>
-                  <div className="photo-canvas">
-                      <img src={photo2023} className="photo" alt="photo" />
-                      <div className="photo-caption">VisLab {year.toString()}</div>
-                  </div>
-                  <span id="right-arrow">{mSVG}</span>
-              </div>
-
-              <div className="chart-container">
-                  <div className="chart">
-                      <LineChart currentYear={year}/>
-                  </div>
-                  <div className="slider-container">
-                      <Slider currentYear={year}></Slider>
-                  </div>
-              </div>
-          </div>
-      {/* <h1>PEP : READY, GET SET, GO!</h1>
-
-      <div className="control-buttons">
-        {started === null && (
-          <button
-            onClick={() => {
-              setStarted("aframe");
-            }}
-          >
-            Start Tracking
-          </button>
-        )}
-        {started !== null && (
-          <button
-            onClick={() => {
-              setStarted(null);
-            }}
-          >
-            Stop
-          </button>
-        )}
+          <img src={logo} className="heading-logo" alt="logo" />
+          <div className="heading-title">VisLab 20th Anniversary</div>
       </div>
+      <div className="canvas">
+          <div className="photo-container">
+            <span id="left-arrow">{mSVG}</span>
+            <div className="photo-canvas">
+              <img src={photo2023} className="photo"  alt="image"/>
+              <div className="photo-caption">VisLab {year.toString()}</div>
+              <Stack spacing={2} sx={{display: "flex", alignItems: "center"}}>
+                <Typography>Page: {page}</Typography>
+                <Pagination count={10} page={page} onChange={handleChange} />
+              </Stack>
+            </div>
+            <span id="right-arrow">{mSVG}</span>
+          </div>
 
-      {started === "aframe" && (
-        <div className="container">
-          <ArComponent />
-          <video></video>
-        </div>
-      )} */}
+          <div className="chart-container">
+              <div className="chart">
+                  <LineChart currentYear={year}/>
+              </div>
+              <div className="slider-container">
+                  <Slider currentYear={year}></Slider>
+              </div>
+          </div>
+      </div>
     </div>
   );
 };
